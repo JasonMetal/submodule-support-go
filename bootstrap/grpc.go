47484057c6a440c283f88eba215e3ae886a75fa3
wuweichi <wwc@many-idea.cloud>
2022-06-02 09:54:27 +0800

    fix grpc server

--- bootstrap/grpc.go
+++ bootstrap/grpc.go
@@ -2,6 +2,9 @@ package bootstrap
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"gitee.com/DXTeam/idea-go.git/helper/logger"
 	"gitee.com/DXTeam/idea-go.git/helper/number"
 	"github.com/gin-gonic/gin"
 	grpcMiddleware "github.com/grpc-ecosystem/go-grpc-middleware"
@@ -12,6 +15,12 @@ import (
 	_ "google.golang.org/grpc/encoding/gzip"
 	"google.golang.org/grpc/keepalive"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/reflection"
+	"net"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
 	"time"
 )
 
@@ -93,4 +102,69 @@ func setTraceCtx(ctx *gin.Context) context.Context {
 
 func panicLog(err error) {
 	CoreCtx.Logger.Error(err.Error(), zap.String("type", "grpc"))
+}
+
+func RunServer(s *grpc.Server, addr string) {
+	// register reflection
+	registerGrpcReflection(s)
+
+	if runtime.GOOS == "windows" {
+		l, err := net.Listen("tcp", addr)
+		if err != nil {
+			logger.Error("grpc", zap.NamedError("grpc", errors.New("grpc error"+err.Error())))
+		}
+
+		if err != nil {
+			os.Exit(1)
+		}
+
+		go s.Serve(l)
+		gracefulGrpcShutdown(s)
+	} else {
+		// todo 后面加入jpillora/oversee平滑重启
+
+		//overseer.Run(overseer.Config{
+		//	//Debug:            true,
+		//	TerminateTimeout: 120 * time.Second,
+		//	Program: func(state overseer.State) {
+		//		s.Serve(state.Listener)
+		//	},
+		//	Address: addr,
+		//})
+
+		l, err := net.Listen("tcp", addr)
+		if err != nil {
+			logger.Error("grpc", zap.NamedError("grpc", errors.New("grpc error"+err.Error())))
+		}
+
+		if err != nil {
+			os.Exit(1)
+		}
+
+		go s.Serve(l)
+		gracefulGrpcShutdown(s)
+
+	}
+}
+
+// registerGrpcReflection 用户本地调试
+func registerGrpcReflection(s *grpc.Server) {
+	if DevEnv == EnvLocal {
+		reflection.Register(s)
+	}
+}
+
+func gracefulGrpcShutdown(s *grpc.Server) {
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT, os.Interrupt)
+	getSignal := <-ch
+	if getSignal == syscall.SIGTERM || getSignal == syscall.SIGQUIT || getSignal == syscall.SIGINT {
+
+		logger.Debug("grpc", zap.NamedError("shutdown", errors.New("grpc server shutdown, handle signal "+fmt.Sprint(getSignal))))
+
+		time.Sleep(2 * time.Minute)
+	}
+	s.GracefulStop()
+	s.Stop()
+	os.Exit(0)
 }
