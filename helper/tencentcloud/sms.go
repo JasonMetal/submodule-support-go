256c531508f23d078fad72314712ab5d0a0b883c
张世杰 <1095143545@qq.com>
2022-06-08 16:55:40 +0800

    调整短信发送helper内容

--- helper/tencentcloud/sms.go
+++ helper/tencentcloud/sms.go
@@ -23,31 +23,34 @@ func (s *SmsParams) Send() (b []byte, err error) {
 		c.Request.SignName = common.StringPtr(s.SignName)
 	}
 	if s.TemplateId == "" {
-		err := errors.New("模板ID不能为空!")
+		err = errors.New("模板ID不能为空!")
 		return nil, err
 	}
 	if len(s.Mobile) == 0 {
-		err := errors.New("手机号不能为空!")
+		err = errors.New("手机号不能为空!")
 		return nil, err
 	}
+	//防止因切片而修改原手机号的值
+	var mobileList = make([]string, 0)
 
 	// 处理手机号 自动兼容 带+号的手机
-	for key, value := range s.Mobile {
-		//如果存在+号则跳过
+	for _, value := range s.Mobile {
+		//如果存在+号则直接放进去
 		if strings.Index(value, "+") >= 0 {
-			continue
+			mobileList = append(mobileList, value)
+		} else {
+			mobileList = append(mobileList, c.MobileCode+value)
 		}
-		s.Mobile[key] = c.MobileCode + value
 	}
 	//去重手机号
-	s.Mobile = slices.Unique(s.Mobile)
+	mobileList = slices.Unique(mobileList)
 	// 处理模板参数
 	if len(s.TemplateParams) > 0 {
 		c.Request.TemplateParamSet = common.StringPtrs(s.TemplateParams)
 	}
 	// 处理模板ID
 	c.Request.TemplateId = common.StringPtr(s.TemplateId)
-	c.Request.PhoneNumberSet = common.StringPtrs(s.Mobile)
+	c.Request.PhoneNumberSet = common.StringPtrs(mobileList)
 	response, err := c.Client.SendSms(c.Request)
 
 	b, err = json.Marshal(response.Response)
