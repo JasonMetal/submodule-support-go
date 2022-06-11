7b055f3a033b1dfa84160eda709576dc60d5422c
wuweichi <wwc@many-idea.cloud>
2022-06-11 17:19:21 +0800

    add X-User-Id to Access-Control-Allow-Headers

--- bootstrap/app.go
+++ bootstrap/app.go
@@ -153,7 +153,7 @@ func ControlCors() gin.HandlerFunc {
 		if strings.Contains(origin, "manyidea.cloud") {
 			context.Header("Access-Control-Allow-Origin", origin)
 			context.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-			context.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Token, X-Token")
+			context.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Token, X-Token, X-User-Id")
 			context.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
 			context.Header("Access-Control-Allow-Credentials", "true")
 
